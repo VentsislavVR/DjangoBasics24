57aace3ab00859faf4eb1d151a3d125da0c8c32c
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2024-01-10 22:21:15 +0200

    added  T/templates

--- 01.django_introduction/django101/django101/tasks/views.py
+++ 01.django_introduction/django101/django101/tasks/views.py
@@ -1,5 +1,6 @@
-import http.client
+
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from django101.tasks.models import Task
 
@@ -15,29 +16,43 @@ from django101.tasks.models import Task
     # 'Content-Type: application/json',
 
 def index(request):
-    title_filter = request.GET.get("filter", None)
+    # title_filter = request.GET.get("filter", None)
+    # tasks = Task.objects.all()
+    # if title_filter:
+    #     tasks = tasks.filter(title__icontains=title_filter)
+    #
+    # if not tasks:
+    #     return HttpResponse(
+    #     "<h1>No tasks!!!</h1>"
+    # )
+    # result = []
+    # for task in tasks:
+    #     result.append(f"""
+    #     <li>
+    #     <h2>{task.title}</h2>
+    #     <p>{task.description}</p>
+    #     </li>
+    #     """
+    #     )
+    # ul = f"<ul>{''.join(result)}</ul>"
+    #
+    # content = f"""
+    # <h1>{len(tasks)}</h1>
+    # {ul}
+    # """
+    title_filter = request.GET.get("title_filter", '')
     tasks = Task.objects.all()
     if title_filter:
         tasks = tasks.filter(title__icontains=title_filter)
+    context = {
+        'title': 'Hello World this is THE TASKS APP',
+        'tasks': tasks,
+        'task_count': tasks.count(),
+        'title_filter': title_filter
+    }
 
-    if not tasks:
-        return HttpResponse(
-        "<h1>No tasks!!!</h1>"
-    )
-    result = []
-    for task in tasks:
-        result.append(f"""
-        <li>
-        <h2>{task.title}</h2>
-        <p>{task.description}</p>
-        </li>
-        """
-        )
-    ul = f"<ul>{''.join(result)}</ul>"
-
-    content = f"""
-    <h1>{len(tasks)}</h1>
-    {ul}
-    """
-
-    return HttpResponse(content)
+    # return HttpResponse(content)
+    return render(request,
+                  "tasks/index.html",
+                  context
+                  )
